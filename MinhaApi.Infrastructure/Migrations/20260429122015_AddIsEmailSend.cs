6240259f0230e226e66d76d9bbd58261491701cc
alebortone <alexandrebortone2015@gmail.com>
2026-04-29 09:26:28 -0300

    feat: add IsEmailSent migration and update database

---
+++ MinhaApi.Infrastructure/Migrations/20260429122015_AddIsEmailSend.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinhaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsEmailSend : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailSend",
+                table: "employee",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEmailSend",
+                table: "employee");
+        }
+    }
+}
